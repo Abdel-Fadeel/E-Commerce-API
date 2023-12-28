abac410c429e33d99c8b7b7837386927094ff111
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-12-28 03:53:36 +0200

    Add register

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,5 +1,27 @@
 import { Handler } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/UserModel.js';
+import { BadRequestError } from '../errors/customErrors.js';
 
-export const register: Handler = async () => {};
-export const login: Handler = async () => {};
-export const logout: Handler = async () => {};
+// Register New User
+export const register: Handler = async (req, res) => {
+  const { name, email, password } = req.body;
+  // Check for existing emails
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) throw new BadRequestError('Email already exists');
+  // Set role dynamically (first registered user is an admin)
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  const role = isFirstAccount ? 'admin' : 'user';
+  // Create User
+  const user = await User.create({ name, email, password, role });
+
+  res.status(StatusCodes.CREATED).json({ user });
+};
+// Login User
+export const login: Handler = async (req, res) => {
+  res.send('Login');
+};
+// Logout User
+export const logout: Handler = async (req, res) => {
+  res.send('Logout');
+};
--- models/UserModel.ts
+++ models/UserModel.ts
@@ -23,7 +23,7 @@ const userSchema = new Schema({
   },
   role: {
     type: String,
-    enum: ['admin,user'],
+    enum: ['admin', 'user'],
     default: 'user',
   },
 });
