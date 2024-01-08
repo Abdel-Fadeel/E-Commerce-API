199aaec104111d9689445d7411c4da66d1ca6d77
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2024-01-08 21:00:10 +0200

    Add login handler

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,7 +1,11 @@
 import { Handler } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
-import { BadRequestError } from '../utils/customErrors.js';
+import {
+  BadRequestError,
+  UnauthenticatedError,
+  UnauthorizedError,
+} from '../utils/customErrors.js';
 import { attachCookiesToResponse } from '../utils/jwt.js';
 
 // Register New User
@@ -21,11 +25,24 @@ export const register: Handler = async (req, res) => {
 
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
+
 // Login User
 export const login: Handler = async (req, res) => {
-  res.send('Login');
+  const { email, password } = req.body;
+  if (!email || !password)
+    throw new BadRequestError('Please provide email and password');
+
+  const user = await User.findOne({ email });
+  if (!user) throw new UnauthenticatedError('Invalid credentials');
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) throw new UnauthenticatedError('Invalid credentials');
+
+  const tokenUser = { name: user.name, userId: user.id, role: user.role };
+  attachCookiesToResponse(res, tokenUser);
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
+
 // Logout User
-export const logout: Handler = async (req, res) => {
-  res.send('Logout');
-};
+export const logout: Handler = async (req, res) => {};
